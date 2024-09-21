67b64ecd1e5f5061dfd58c24380f3a018076a2eb
Julian <julianferegotti96@gmail.com>
2024-09-21 15:27:52 -0300

    refactor

--- proxy-server/src/index.js
+++ proxy-server/src/index.js
@@ -3,78 +3,118 @@ import cors from "cors";
 import EventSource from "eventsource";
 import fs from "fs";
 
-const app = express();
+//region DataHandler
+class DataHandler {
+  constructor() {
+    if (DataHandler.instance) {
 
-app.use(cors());
+      return DataHandler.instance;
+    }
 
-function store(img, time) {
-  const newData = {
-    img,
-    time,
-  };
-
-  fs.writeFileSync("./src/data.json", JSON.stringify(newData));
-}
-
-function get() {
-  const data = fs.readFileSync("./src/data.json");
-  return JSON.parse(data);
-}
-
-/*
-  Adapts the response from the original server to the client
-  
-  {"event":"pepito", "time":1725714575,"img":"https://storage.thecatdoor.com/assets/1725714575-in-799154526.jpg"}
-*/
-const adaptReponse = (payload) => {
-  console.log(payload);
-  if (payload.event === "heartbeat") {
-    const data = get();
-
-    return JSON.stringify({
-      event: "heartbeat",
-      time: payload.time,
-      img: data.img,
-    });
+    this.data = JSON.parse(fs.readFileSync("./src/data.json"));
+    this.newData = false;
+    DataHandler.instance = this;
+    return this;
   }
 
-  return JSON.stringify(payload);
-};
+  store(img, time) {
+    console.log('Storing new data', { img, time })
+    const newData = {
+      img,
+      time,
+    };
+
+    fs.writeFileSync("./src/data.json", JSON.stringify(newData));
+    this.newData = true;
+    this.data = newData;
+  }
+
+  get() {
+    if (!this.newData) {
+      return this.data;
+    }
+
+    const data = fs.readFileSync("./src/data.json");
+    this.newData = false;
+    return JSON.parse(data);
+  }
+
+  hasNewData() {
+    return this.newData;
+  }
+}
+//endregion
+//region EventHandler
+class EventHandler {
+  constructor(dataHandler = new DataHandler()) {
+    this.dataHandler = dataHandler;
+  }
+
+  handle(event) {
+    console.log('NEW EVENT RECEIVED', event.data)
+    const payload = JSON.parse(event.data);
+
+
+    if (payload.event === "pepito") {
+      if (payload.type === "in" || payload.type === "out") {
+        this.dataHandler.store(payload.img, payload.time);
+      }
+    }
+  }
+}
+//endregion
+//region Constants
+
+const SSE_URL = "https://api.thecatdoor.com/sse/v1/events";
+const app = express();
+const dataHandler = new DataHandler();
+const eventHandler = new EventHandler(dataHandler);
+
+
+//endregion
+//region Server
+app.use(cors());
+
+
 
 app.get("/sse-proxy", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
 
-  const sseUrl = "https://api.thecatdoor.com/sse/v1/events";
-  const eventSource = new EventSource(sseUrl);
 
-  eventSource.onmessage = (event) => {
-    const payload = JSON.parse(event.data);
+  const interval = setInterval(() => {
+    const data = dataHandler.get();
 
-    if (payload.event === "pepito") {
-      if (payload.type === "in" || payload.type === "out") {
-        store(payload.img, payload.time);
-      }
-    }
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+    return
 
-    const data = adaptReponse(payload);
-    console.log(data);
-    res.write(`data: ${data}\n\n`);
-  };
+  }, 8000);
 
-  eventSource.onerror = (err) => {
-    console.error("Error en la conexión SSE:", err);
-    res.write("data: Error en la conexión SSE\n\n");
-    eventSource.close();
-  };
 
   req.on("close", () => {
-    eventSource.close();
     res.end();
+    clearInterval(interval);
   });
 });
 
-app.listen(3000, () => {
-  console.log("Proxy escuchando en http://localhost:3000");
+app.get("/health", (_, res) => res.send('im alive'))
+
+
+app.listen(3001, () => {
+
+  console.log("Server listening on port 3000");
+  const eventSource = new EventSource(SSE_URL);
+
+  eventSource.onopen = () => {
+    console.log("Connected to EventSource");
+  }
+
+  eventSource.onerror = (err) => {
+    console.error("Error en la conexión SSE:", err);
+  }
+
+  eventSource.onmessage = (event) => {
+    eventHandler.handle(event);
+  }
 });
